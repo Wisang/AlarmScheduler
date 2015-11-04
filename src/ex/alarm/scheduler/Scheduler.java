1aa43d6ff83f77ce63f24b8c73eb5e0757057161
unknown <wisang.eom@WMRRD11-NA10081.LGE.NET>
2015-11-04 16:11:47 +0900

    given when then completed

--- src/ex/alarm/scheduler/Scheduler.java
+++ src/ex/alarm/scheduler/Scheduler.java
@@ -1,13 +1,36 @@
 package ex.alarm.scheduler;
 
+import ex.alarm.driver.AlarmAlert;
+import ex.os.service.TimeService;
+
 public class Scheduler {
 
+	private TimeService timeService;
+	private AlarmAlert alarmAlert;
+	private int day;
+	private int minute;
+	private boolean isScheduled = false;
+	
+	public Scheduler(TimeService timeService,
+			AlarmAlert alarmAlert) {
+		this.timeService = timeService;
+		this.alarmAlert = alarmAlert;
+	}
+
 	public void wakeUp() {
+		// if schedule is not added then return;
+		if(!isScheduled )
+			return;
 		
+		if(timeService.getCurrentDay() == day &&
+				timeService.getCurrentMinute() == minute)
+			alarmAlert.startAlarm();
 	}
 
 	public void add(int day, int minute) {
-		
+		isScheduled = true;
+		this.day = day;
+		this.minute = minute;
 	}
 
 }
--- test/ex/alarm/scheduler/SchedulerTest.java
+++ test/ex/alarm/scheduler/SchedulerTest.java
@@ -1,45 +1,69 @@
 package ex.alarm.scheduler;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import ex.alarm.driver.AlarmAlertSpy;
-import ex.os.service.FakeTimeService;
+import ex.alarm.driver.AlarmAlert;
+import ex.os.service.TimeService;
 
 public class SchedulerTest {
 	private static final int MONDAY = 1;
+	private static final int TUESDAY = 2;
 	private Scheduler scheduler;
-	private AlarmAlertSpy alarmAlertSpy;
-	private FakeTimeService fakeTimeService;
+	private AlarmAlert alarmAlertSpy;
+	private TimeService fakeTimeService;
 	
 	@Before
 	public void setUp(){
-		scheduler = new Scheduler();
-		alarmAlertSpy = new AlarmAlertSpy();
-		fakeTimeService = new FakeTimeService();
+		alarmAlertSpy = mock(AlarmAlert.class); //new AlarmAlertSpy();
+		fakeTimeService = mock(TimeService.class);//new FakeTimeService();
+		scheduler = new Scheduler(fakeTimeService, alarmAlertSpy);
 	}
 
 	@Test
 	public void doesNotAlarmWhenNoScheduleAdded() throws Exception {
 		scheduler.wakeUp();
-		assertFalse(alarmAlertSpy.isAlarmed());
+		thenItshoudNotAlarm();
 	}
 	
 	@Test
 	public void doesNotAlarmWhenItsNotTheTimeYet() throws Exception {
-		scheduler.add(MONDAY, 7*60);
-		fakeTimeService.setTime(MONDAY, 6*60);
-		scheduler.wakeUp();
-		assertFalse(alarmAlertSpy.isAlarmed());
+		givenTheScheduleIsAddedAs(MONDAY, 7*60);
+		whenItBecomesTimeAs(MONDAY, 6*60);
+		thenItshoudNotAlarm();
 	}
 	
 	@Test
 	public void alarmWhenItsTime() throws Exception {
-		scheduler.add(MONDAY, 7*60);
-		fakeTimeService.setTime(MONDAY, 7*60);
+		givenTheScheduleIsAddedAs(MONDAY, 7*60);
+		whenItBecomesTimeAs(MONDAY, 7*60);
+		thenItShoudAlarm();
+	}
+	
+	@Test
+	public void doesNotAlarmWhenItsNotTheDay() throws Exception {
+		givenTheScheduleIsAddedAs(MONDAY, 7*60);
+		whenItBecomesTimeAs(TUESDAY, 7*60);
+		thenItshoudNotAlarm();
+	}
+
+	private void whenItBecomesTimeAs(int day, int minute) {
+		when(fakeTimeService.getCurrentDay()).thenReturn(day);
+		when(fakeTimeService.getCurrentMinute()).thenReturn(minute);
 		scheduler.wakeUp();
-		assertTrue(alarmAlertSpy.isAlarmed());
+	}
+
+	private void givenTheScheduleIsAddedAs(int day, int minute) {
+		scheduler.add(day, minute);
+	}
+	
+	private void thenItshoudNotAlarm() {
+		verify(alarmAlertSpy, never()).startAlarm();
+	}
+
+	private void thenItShoudAlarm() {
+		verify(alarmAlertSpy).startAlarm();
 	}
 }
